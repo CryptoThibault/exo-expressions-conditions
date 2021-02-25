2a8d9ecb40a1135f98bb613397ce2377edc51ca4
CryptoThibault <thibault.chalaoux@gmail.com>
2021-02-25 18:34:24 +0100

    Chalenge

---
+++ elevator.js
@@ -0,0 +1,31 @@
+let currentFloor = 0
+let targetFloor = 0
+if (currentFloor !== targetFloor) {
+  currentFloor = targetFloor
+}
+
+
+switch (currentFloor) {
+  case -2: console.log(`Vous étes à l'étage ${currentFloor}.`)
+    break
+  case -1: console.log(`Vous étes à l'étage ${currentFloor}.`)
+    break
+  case 0: console.log(`Vous étes au rez de chaussée.`)
+    break
+  case 1: console.log(`Vous êtes au ${currentFloor}er étage.`)
+    break
+  case 2: console.log(`Vous êtes au ${currentFloor}éme étage.`)
+    break
+  case 3: console.log(`Vous êtes au ${currentFloor}éme étage.`)
+    break
+  case 4: console.log(`Vous êtes au ${currentFloor}éme étage.`)
+    break
+  case 5: console.log(`Vous êtes au ${currentFloor}éme étage.`)
+    break
+  case 6: console.log(`Vous êtes au ${currentFloor}éme étage.`)
+    break
+  case 7: console.log(`Vous êtes au ${currentFloor}éme étage.`)
+    break
+  default: console.log(`Cette étage n'existe pas.`)
+}
+
